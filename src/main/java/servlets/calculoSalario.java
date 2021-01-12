9d0dc8f04abc59d71ea018fd2468a24ff0ac82c6
Jaime Sanchez <>
2021-01-12 18:33:03 +0100

    Version 1.0 Calculo de la retención del IVA de un salario bruto

---
+++ src/main/java/servlets/calculoSalario.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+public class calculoSalario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     
+    float tipoIva;
+    
+     public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+        tipoIva=Float.parseFloat(config.getInitParameter("tipoIva"));
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String nombreEmpleado = request.getParameter("nombre");
+            float salarioBruto = Float.parseFloat(request.getParameter("salarioBruto"));
+            float salarioNeto = salarioBruto - (salarioBruto*tipoIva/100);
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet calculoSalario</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Calculo IVA</h1>");
+            out.println("<h2>Nombre del empleado/a "+nombreEmpleado+"</h2>");
+            out.println("<h2>Tipo de retención "+tipoIva+"</h2>");
+            out.println("<h2>Salario Bruto "+salarioBruto+"</h2>");
+            out.println("<h2>Salario Neto "+salarioNeto+"</h2>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+         <h1>Calculo del IVA</h1>
+        <form action="calculoSalario">
+            Nombre del empleado/a: <input type="text" name="nombre"><br>
+            Salario bruto: <input type="text" name="salarioBruto"><br>
+            <input type="submit" value="Calcula IVA">
+        </form>
+    </body>
+</html>
